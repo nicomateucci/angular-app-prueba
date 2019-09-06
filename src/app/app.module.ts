1fc88896712cec159892be8969e09a7aa24de5d5
nicomateucci <nico-mateucci@hotmail.com>
2019-09-06 18:26:07 -0300

    Agregue componentes alumnos y maestros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AlumnoComponent } from './alumno/alumno.component';
+import { MaestroComponent } from './maestro/maestro.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AlumnoComponent, MaestroComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
